47a6c3cd0b76d2d2dee2cf6b0f90040eb0209790
para12 <hyunwooda@gmail.com>
2023-09-25 15:32:51 +0900

    image preload

--- app/common/cloudinaryImage.tsx
+++ app/common/cloudinaryImage.tsx
@@ -1,11 +1,27 @@
 "use client";
 
 import { CldImage } from "next-cloudinary";
+import { useState } from "react";
 
 export default function CloudinaryImage(props: any) {
+  const [imageReady, setImageReady] = useState(true);
   return (
     <div>
-      <CldImage {...props} />
+      {imageReady && (
+        <CldImage
+          {...props}
+          effects={[{ blur: "3000" }]}
+          style={{ zIndex: "1" }}
+          className="absolute"
+        />
+      )}
+      {
+        <CldImage
+          {...props}
+          style={{ zIndex: "0" }}
+          onLoadingComplete={() => setImageReady(false)}
+        />
+      }
     </div>
   );
 }
